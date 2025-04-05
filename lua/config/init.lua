b80f95cd383a92c39b735a6b739fe9d00934537d
Wahyu <wahyu@bti.id>
2025-04-06 00:21:36 +0700

    dev: repair completion

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -75,6 +75,49 @@ require('trouble').setup()
 -- Commenting
 require('mini.comment').setup()
 
+-- Completion
+local luasnip = require("luasnip")
+
+-- Completion plugin setup
+local cmp = require("cmp")
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    -- Confirm selection
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+
+    -- TAB to select next item or jump in snippet
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+
+    -- Shift-TAB to select previous item or jump back in snippet
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  }),
+})
+
 -- Conform Format on Save
 require("conform").setup({
   formatters_by_ft = {
--- lua/plugins/snack.lua
+++ lua/plugins/snack.lua
@@ -6,6 +6,7 @@ return {
   opts = {
     bigfile = { enabled = true },
     bufdelete = { enabled = true },
+    dim = { enabled = true },
     dashboard = {
       width = 60,
       row = nil,                                                                   -- dashboard position. nil for center
@@ -72,12 +73,116 @@ return {
       },
     },
     picker = {
-      sources = {
-        explorer = {
-          -- your explorer picker configuration comes here
-          -- or leave it empty to use the default settings
-        }
-      }
+      prompt = " ",
+      sources = {},
+      focus = "input",
+      layout = {
+        cycle = true,
+        --- Use the default layout or vertical if the window is too narrow
+        preset = function()
+          return vim.o.columns >= 120 and "default" or "vertical"
+        end,
+      },
+      ---@class snacks.picker.matcher.Config
+      matcher = {
+        fuzzy = true,          -- use fuzzy matching
+        smartcase = true,      -- use smartcase
+        ignorecase = true,     -- use ignorecase
+        sort_empty = false,    -- sort results when the search string is empty
+        filename_bonus = true, -- give bonus for matching file names (last part of the path)
+        file_pos = true,       -- support patterns like `file:line:col` and `file:line`
+        -- the bonusses below, possibly require string concatenation and path normalization,
+        -- so this can have a performance impact for large lists and increase memory usage
+        cwd_bonus = false,     -- give bonus for matching files in the cwd
+        frecency = false,      -- frecency bonus
+        history_bonus = false, -- give more weight to chronological order
+      },
+      sort = {
+        -- default sort is by score, text length and index
+        fields = { "score:desc", "#text", "idx" },
+      },
+      ui_select = true, -- replace `vim.ui.select` with the snacks picker
+      ---@class snacks.picker.formatters.Config
+      formatters = {
+        text = {
+          ft = nil, ---@type string? filetype for highlighting
+        },
+        file = {
+          filename_first = false, -- display filename before the file path
+          truncate = 40,          -- truncate the file path to (roughly) this length
+          filename_only = false,  -- only show the filename
+          icon_width = 2,         -- width of the icon (in characters)
+          git_status_hl = true,   -- use the git status highlight group for the filename
+        },
+        selected = {
+          show_always = false, -- only show the selected column when there are multiple selections
+          unselected = true,   -- use the unselected icon for unselected items
+        },
+        severity = {
+          icons = true,  -- show severity icons
+          level = false, -- show severity level
+          ---@type "left"|"right"
+          pos = "left",  -- position of the diagnostics
+        },
+      },
+      ---@class snacks.picker.previewers.Config
+      previewers = {
+        diff = {
+          builtin = true,    -- use Neovim for previewing diffs (true) or use an external tool (false)
+          cmd = { "delta" }, -- example to show a diff with delta
+        },
+        git = {
+          builtin = true, -- use Neovim for previewing git output (true) or use git (false)
+          args = {},      -- additional arguments passed to the git command. Useful to set pager options usin `-c ...`
+        },
+        file = {
+          max_size = 1024 * 1024, -- 1MB
+          max_line_length = 500,  -- max line length
+          ft = nil, ---@type string? filetype for highlighting. Use `nil` for auto detect
+        },
+        man_pager = nil, ---@type string? MANPAGER env to use for `man` preview
+      },
+      ---@class snacks.picker.jump.Config
+      jump = {
+        jumplist = true,   -- save the current position in the jumplist
+        tagstack = false,  -- save the current position in the tagstack
+        reuse_win = false, -- reuse an existing window if the buffer is already open
+        close = true,      -- close the picker when jumping/editing to a location (defaults to true)
+        match = false,     -- jump to the first match position. (useful for `lines`)
+      },
+      toggles = {
+        follow = "f",
+        hidden = "h",
+        ignored = "i",
+        modified = "m",
+        regex = { icon = "R", value = false },
+      },
+      win = {
+        -- input window
+        input = {
+          keys = {
+            -- to close the picker on ESC instead of going to normal mode,
+            -- add the following keymap to your config
+            -- ["<Esc>"] = { "close", mode = { "n", "i" } },
+            ["/"] = "toggle_focus",
+            ["<S-Tab>"] = { "list_up", mode = { "i", "n" } },
+            ["<Tab>"] = { "list_down", mode = { "i", "n" } },
+          },
+          b = {
+            minipairs_disable = true,
+          },
+        },
+        -- result list window
+        -- preview window
+        preview = {
+          keys = {
+            ["<Esc>"] = "cancel",
+            ["q"] = "close",
+            ["i"] = "focus_input",
+            ["<a-w>"] = "cycle_win",
+          },
+        },
+      },
     },
     explorer = { enabled = true },
     indent = { enabled = true },
@@ -164,6 +269,9 @@ return {
         Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
         Snacks.toggle.inlay_hints():map("<leader>uh")
         Snacks.toggle.indent():map("<leader>ug")
+
+        -- initialization config
+        Snacks.dim()
       end,
     })
   end,
